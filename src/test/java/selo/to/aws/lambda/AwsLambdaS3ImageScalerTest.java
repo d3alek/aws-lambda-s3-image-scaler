c8a76dbf3a981af8e2db24e8081dc785958586ee
Aleksandar Kodzhabashev <akodzhabashev@gmail.com>
2015-11-16 14:43:14 +0200

    Testing with big image

--- src/test/java/selo/to/aws/lambda/AwsLambdaS3ImageScalerTest.java
+++ src/test/java/selo/to/aws/lambda/AwsLambdaS3ImageScalerTest.java
@@ -10,19 +10,32 @@ import java.io.InputStream;
 
 import javax.imageio.ImageIO;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
 
 public class AwsLambdaS3ImageScalerTest {
 
-
+  private static final String SCALED_PREFIX = "scaled";
   private static final String EMPTY_IMAGE_PNG = "empty-image.png";
+  private static final String BIG_IMAGE = "big-image.jpg";
   private static final String TEST_BUCKET = "seloto-images-test";
   private String key;
+
+  @Before
+  public void cleanup() {
+    AmazonS3 s3Client = new AmazonS3Client();
+    for (S3ObjectSummary s3ObjectSummary : s3Client.listObjects(TEST_BUCKET).getObjectSummaries()) {
+      if (s3ObjectSummary.getKey().startsWith(SCALED_PREFIX)) {
+        s3Client.deleteObject(TEST_BUCKET, s3ObjectSummary.getKey());
+      }
+    }
+  }
 
   @Test
   public void scaleImageProduces3Sizes() throws Exception {
@@ -33,6 +46,17 @@ public class AwsLambdaS3ImageScalerTest {
     thenImageExists(AwsLambdaS3ImageScaler.PREFIX + "-" + 200 + "-" + EMPTY_IMAGE_PNG, 200);
     thenImageExists(AwsLambdaS3ImageScaler.PREFIX + "-" + 400 + "-" + EMPTY_IMAGE_PNG, 400);
     thenImageExists(AwsLambdaS3ImageScaler.PREFIX + "-" + 800 + "-" + EMPTY_IMAGE_PNG, 800);
+  }
+
+  @Test
+  public void scaleBigImageWorks() throws Exception {
+    givenImage(BIG_IMAGE);
+
+    whenScalingImage();
+
+    thenImageExists(AwsLambdaS3ImageScaler.PREFIX + "-" + 200 + "-" + BIG_IMAGE, 200);
+    thenImageExists(AwsLambdaS3ImageScaler.PREFIX + "-" + 400 + "-" + BIG_IMAGE, 400);
+    thenImageExists(AwsLambdaS3ImageScaler.PREFIX + "-" + 800 + "-" + BIG_IMAGE, 800);
   }
 
   private void thenImageExists(String key, int maxDimension) throws IOException {
